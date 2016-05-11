4aef097e4a63a94264957d96e55527129cb3985a
Wonchul <wonchull@sfu.ca>
2016-05-10 20:10:04 -0700

    Title added dont remove

--- lightTracker.c
+++ lightTracker.c
@@ -1,3 +1,15 @@
+/*
+ *
+ * Title: Line Follower
+ * Author: Anjuchan and Wonchu
+ *
+ * We have to figure out which surface that we will be testing the robot
+ * since the reflected brightness is different based on all different 
+ * materials and places 
+ *
+ * PLEASE DO NOT ERASE 
+ *
+ * */
 #pragma config(Sensor, S1,     lightSensor,    sensorEV3_Color)
 #pragma config(Motor,  motorA,          rightDriveMotor, tmotorEV3_Large, PIDControl, driveRight, encoder)
 #pragma config(Motor,  motorB,          leftDriveMotor, tmotorEV3_Large, PIDControl, driveLeft, encoder)
