6a9ff7e9672f9368027effb294e5018ad5fcf1e4
Anjandev Momi <anjan@momi.ca>
2016-07-04 03:04:56 +0000

    working barcodeReaderTest.m

--- Project3/barcodeReaderTest.m
+++ Project3/barcodeReaderTest.m
@@ -17,15 +17,15 @@ MinPeakHeight = 50;
 
 %% Parsing Data
 
-MyRawImage = imread('Sample_LetterA.jpg');
+MyRawImage = imread('Untitled.jpg');
 MyRawImage = double(MyRawImage);
 
-MyLightVals = MyRawImage(35,:,1);
+MyLightVals = MyRawImage(375,:,1);
 
-MyLightVals = MyLightVals(10:length(MyLightVals)-100);
+MyLightVals = MyLightVals(10:length(MyLightVals));
 
 % read every second value
-%for i=2:2:length(lightDataWithIndices)
+% for i=2:2:length(lightDataWithIndices)
 % lightDataNoIndices(noIndicesIndex)= lightDataWithIndices(i);
 % noIndicesIndex = noIndicesIndex + 1;
 %end
@@ -94,153 +94,155 @@ for i=1:length(widths)
     end
 end
 
+
 %% Clasify
 beginningOfWide = 1;
 outputIndex = 1;
 Letters = [];
 
+
 for i = 1:9:length(wideOrNarrow)
-    if i + 8 <= length(wideOrNarrow)
-        Letter = wideOrNarrow(i:i+8);
+    if i+8+(outputIndex -1) <= length(wideOrNarrow)
+        Letter = wideOrNarrow(i + (outputIndex -1):i+8+(outputIndex -1));
     else
         break
-    end
+    end 
+    
     
     if Letter == ['n', 'n', 'n', 'w', 'w', 'n', 'w', 'n', 'n'];
         s = '0'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'w'];
         s = '1'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'w', 'n', 'n', 'n', 'n', 'w'];
         s = '2'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'w', 'w', 'n', 'n', 'n', 'n', 'n'];
         s = '3'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'w', 'w', 'n', 'n', 'n', 'w'];
         s = '4'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'w', 'w', 'n', 'n', 'n', 'n'];
         s = '5'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'w', 'w', 'n', 'n', 'n', 'n'];
         s = '6'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'w', 'n', 'n', 'w', 'n', 'w'];
         s = '7'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'w', 'n', 'n', 'w', 'n', 'n'];
         s = '8'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'w', 'n', 'n', 'w', 'n', 'n'];
         s = '9'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'w'];
         s = 'A'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'n', 'n', 'w', 'n', 'n', 'w'];
         s = 'B'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'w', 'n', 'n', 'w', 'n', 'n', 'n'];
         s = 'C'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'n', 'w', 'w', 'n', 'n', 'w'];
         s = 'D'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'n', 'w', 'w', 'n', 'n', 'n'];
         s = 'E'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'n', 'w', 'w', 'n', 'n', 'n'];
         s = 'F'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'n', 'n', 'w', 'w', 'n', 'w'];
         s = 'G'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'n', 'n', 'w', 'w', 'n', 'n'];
         s = 'H'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'n', 'n', 'w', 'w', 'n', 'n'];
         s = 'I'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'n', 'w', 'w', 'w', 'n', 'n'];
         s = 'J'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'n', 'n', 'n', 'n', 'w', 'w'];
         s = 'K'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'n', 'n', 'n', 'n', 'w', 'w'];
         s = 'L'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'w', 'n', 'n', 'n', 'n', 'w', 'n'];
         s = 'M'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'n', 'w', 'n', 'n', 'w', 'w'];
         s = 'N'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'n', 'w', 'n', 'n', 'w', 'n'];
         s = 'O'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'n', 'w', 'n', 'n', 'w', 'n'];
         s = 'P'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'w'];
         s = 'Q'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'n'];
         s = 'R'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'w', 'n', 'n', 'n', 'w', 'w', 'n'];
         s = 'S'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'n', 'w', 'n', 'w', 'w', 'n'];
         s = 'T'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'w', 'n', 'n', 'n', 'n', 'n', 'n', 'w'];
         s = 'U'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'w'];
         s = 'V'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'n'];
         s = 'W'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'n', 'n', 'w', 'n', 'n', 'n', 'w'];
         s = 'X'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'w', 'n', 'n', 'w', 'n', 'n', 'n', 'n'];
         s = 'Y'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'w', 'n', 'w', 'n', 'n', 'n', 'n'];
         s = 'Z'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'n', 'n', 'n', 'n', 'w', 'n', 'w'];
         s = '-'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['w', 'w', 'n', 'n', 'n', 'n', 'w', 'n', 'n'];
         s = '/'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'w', 'n', 'n', 'n', 'w', 'n', 'n'];
         s = ' '
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'n', 'w', 'n', 'w', 'n', 'n', 'n'];
         s = '$'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'n', 'w', 'n', 'n', 'n', 'w', 'n'];
         s = '/'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'n', 'n', 'n', 'w', 'n', 'w', 'n'];
         s = '+'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'n', 'n', 'w', 'n', 'w', 'n', 'w', 'n'];
         s = '%'
-        Letters(outputIndex) = s
+          
     elseif Letter == ['n', 'w', 'n', 'n', 'w', 'n', 'w', 'n', 'n'];
         s = '*'
-        Letters(outputIndex) = s
+          
     end
     
     beginningOfWide = beginningOfWide + 9;
     outputIndex = outputIndex + 1;
 end
-Letters
