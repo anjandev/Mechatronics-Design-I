bdfe50479c8290ed5581c65b43a0e2a5b7ec25c0
Wonchul <wonchull@sfu.ca>
2016-07-04 23:23:04 +0000

    added constants to barcode reader

--- Project3/BarcodeReader.m
+++ Project3/BarcodeReader.m
@@ -9,28 +9,26 @@ SAMPLING_TIME=30e-3;
 DATA_COLUMN = 1;
 
 % For moving average
-WINDOW_SIZE = 4;
+WINDOW_SIZE = 30;
 
 % For find peaks
-MinPeakDistance = 50;
+MinPeakDistance = 12;
+MinPeakHeight = 0.5;
+IGNORE_FIRST = 184;
 
 %% Parsing Data
 
-MyData = csvread('Datalog-2.txt');
+MyData = csvread('datalog-2.txt');
 lightDataWithIndices= MyData(INDEX_TO_START_AT:end, DATA_COLUMN);
 
 noIndicesIndex = 1;
 
 % read every second value
-%for i=2:2:length(lightDataWithIndices)
-% lightDataNoIndices(noIndicesIndex)= lightDataWithIndices(i);
-% noIndicesIndex = noIndicesIndex + 1;
-%end
-
-% For Test
-for i=1:1:length(lightDataWithIndices)-630
- lightDataNoIndices(i)= lightDataWithIndices(i);
+for i=IGNORE_FIRST:2:length(lightDataWithIndices)
+ lightDataNoIndices(noIndicesIndex)= lightDataWithIndices(i);
+ noIndicesIndex = noIndicesIndex + 1;
 end
+
 
 %% Moving Average
 
@@ -47,7 +45,7 @@ end
 
 %before it was MAXPEAKDIST
 
-[pks,locs]= findpeaks(MyLightVals_avg_der,'MinPeakDistance', MinPeakDistance);
+[pks,locs]= findpeaks(MyLightVals_avg_der,'MinPeakDistance', MinPeakDistance, 'MinPeakHeight', MinPeakHeight);
 
 % Shift locs over by one because everything while drawing is zero indexed
 % IE time=0:SAMPLING_TIME:(length(MovingAvg)-1)*SAMPLING_TIME;
@@ -65,24 +63,24 @@ end
 % NOTE: WIDTHS ARE NOT IN SAMPLING TIME, THEYRE IN INDEX DIFFERENCES
 %% Displaying Data
 
-test=length(MyLightVals_avg_der) 
-
 time=0:SAMPLING_TIME:(length(MovingAvg)-1)*SAMPLING_TIME;
 timeNoIndices = 0:SAMPLING_TIME:(length(lightDataNoIndices)-1)*SAMPLING_TIME;
 timeDer = 0:SAMPLING_TIME:(length(MyLightVals_avg_der)-1)*SAMPLING_TIME;
 timePks = locs*SAMPLING_TIME;
 
+subplot(2,1,1)
 plot(timeNoIndices, lightDataNoIndices)
 hold on
 plot(time, MovingAvg,'r')
 hold on
-plot(timeDer, MyLightVals_avg_der,'k') %in green
+subplot(2,1,2)
+plot(timeDer, MyLightVals_avg_der,'k') %in green derivative
 hold on
-plot(timePks,pks,'or') %o is circle, r is red
+plot(timePks,pks,'or') %o is circle, r is red % show peaks
 
 %% Wide and narrow finder
 
-averageWidth = sum(widths) / length(widths);
+averageWidth = (max(widths) + min(widths)) / 2;
 wideOrNarrow = [];
 
 for i=1:length(widths)
@@ -93,102 +91,155 @@ for i=1:length(widths)
     end
 end
 
-%% clasify
-beginningOfWide = 1;
 
-for i = 9: 9:length(wideOrNarrow)
-    if wideOrNarrow(beginningOfWide:i) == ['n', 'n', 'n', 'w', 'w', 'n', 'w', 'n', 'n']
-        s = 'zero ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnwnnnnw')
-        s = '1 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwwnnnnw')
-        s = '2 ';
-    elseif strcmp((beginningOfWide:i), 'wnwwnnnnn')
-        s = '3 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnwwnnnw')
-        s = '4 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnwwnnnn')
-        s = '5 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwwwnnnn')
-        s = '6 '; 
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnwnnwnw')
-        s = '7 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnwnnwnn')
-        s = '8 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwwnnwnn')
-        s = '9 ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnnnwnnw')
-        s = 'A ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwnnwnnw')
-        s = 'B ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnwnnwnnn')
-        s = 'C ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnnwwnnw')
-        s = 'D ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnnwwnnn')
-        s = 'E ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwnwwnnn')
-        s = 'F ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnnnwwnw')
-        s = 'G ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnnnwwnn')
-        s = 'H ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwnnwwnn')
-        s = 'I ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnnwwwnn')
-        s = 'J ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnnnnnww')
-        s = 'K ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwnnnnww')
-        s = 'L ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnwnnnnwn')
-        s = 'M ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnnwnnww')
-        s = 'N ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnnwnnwn')
-        s = 'O ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwnwnnwn')
-        s = 'P ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnnnnwww')
-        s = 'Q ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wnnnnnwwn')
-        s = 'R ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnwnnnwwn')
-        s = 'S ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nnnnwnwwn')
-        s = 'T ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wwnnnnnnw')
-        s = 'U ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwwnnnnnw')
-        s = 'V ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wwwnnnnnn')
-        s = 'W ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwnnwnnnw')
-        s = 'X ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wwnnwnnnn')
-        s = 'Y ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwwnwnnnn')
-        s = 'Z ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwnnnnwnw')
-        s = 'Hyp ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'wwnnnnwnn')
-        s = 'Per ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwwnnnwnn')
-        s = 'Space ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwnwnwnnn')
-        s = 'Dollar ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwnwnnnwn')
-        s = 'Slash ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i), 'nwnnnwnwn')
-        s = 'Plus ';
-    elseif strcmp(wideOrNarrow(beginningOfWide:i),  'nnnwnwnwn')
-        s = 'Mod ';
+%% Clasify
+beginningOfWide = 1;
+outputIndex = 1;
+Letters = [];
+
+
+for i = 1:9:length(wideOrNarrow)
+    if i+8+(outputIndex -1) <= length(wideOrNarrow)
+        Letter = wideOrNarrow(i + (outputIndex -1):i+8+(outputIndex -1));
+    else
+        break
+    end 
+    
+    
+    if Letter == ['n', 'n', 'n', 'w', 'w', 'n', 'w', 'n', 'n'];
+        s = '0'
+          
+    elseif Letter == ['w', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'w'];
+        s = '1'
+          
+    elseif Letter == ['n', 'n', 'w', 'w', 'n', 'n', 'n', 'n', 'w'];
+        s = '2'
+          
+    elseif Letter == ['w', 'n', 'w', 'w', 'n', 'n', 'n', 'n', 'n'];
+        s = '3'
+          
+    elseif Letter == ['n', 'n', 'n', 'w', 'w', 'n', 'n', 'n', 'w'];
+        s = '4'
+          
+    elseif Letter == ['w', 'n', 'n', 'w', 'w', 'n', 'n', 'n', 'n'];
+        s = '5'
+          
+    elseif Letter == ['n', 'n', 'w', 'w', 'w', 'n', 'n', 'n', 'n'];
+        s = '6'
+          
+    elseif Letter == ['n', 'n', 'n', 'w', 'n', 'n', 'w', 'n', 'w'];
+        s = '7'
+          
+    elseif Letter == ['w', 'n', 'n', 'w', 'n', 'n', 'w', 'n', 'n'];
+        s = '8'
+          
+    elseif Letter == ['n', 'n', 'w', 'w', 'n', 'n', 'w', 'n', 'n'];
+        s = '9'
+          
+    elseif Letter == ['w', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'w'];
+        s = 'A'
+          
+    elseif Letter == ['n', 'n', 'w', 'n', 'n', 'w', 'n', 'n', 'w'];
+        s = 'B'
+          
+    elseif Letter == ['w', 'n', 'w', 'n', 'n', 'w', 'n', 'n', 'n'];
+        s = 'C'
+          
+    elseif Letter == ['n', 'n', 'n', 'n', 'w', 'w', 'n', 'n', 'w'];
+        s = 'D'
+          
+    elseif Letter == ['w', 'n', 'n', 'n', 'w', 'w', 'n', 'n', 'n'];
+        s = 'E'
+          
+    elseif Letter == ['n', 'n', 'w', 'n', 'w', 'w', 'n', 'n', 'n'];
+        s = 'F'
+          
+    elseif Letter == ['n', 'n', 'n', 'n', 'n', 'w', 'w', 'n', 'w'];
+        s = 'G'
+          
+    elseif Letter == ['w', 'n', 'n', 'n', 'n', 'w', 'w', 'n', 'n'];
+        s = 'H'
+          
+    elseif Letter == ['n', 'n', 'w', 'n', 'n', 'w', 'w', 'n', 'n'];
+        s = 'I'
+          
+    elseif Letter == ['n', 'n', 'n', 'n', 'w', 'w', 'w', 'n', 'n'];
+        s = 'J'
+          
+    elseif Letter == ['w', 'n', 'n', 'n', 'n', 'n', 'n', 'w', 'w'];
+        s = 'K'
+          
+    elseif Letter == ['n', 'n', 'w', 'n', 'n', 'n', 'n', 'w', 'w'];
+        s = 'L'
+          
+    elseif Letter == ['w', 'n', 'w', 'n', 'n', 'n', 'n', 'w', 'n'];
+        s = 'M'
+          
+    elseif Letter == ['n', 'n', 'n', 'n', 'w', 'n', 'n', 'w', 'w'];
+        s = 'N'
+          
+    elseif Letter == ['w', 'n', 'n', 'n', 'w', 'n', 'n', 'w', 'n'];
+        s = 'O'
+          
+    elseif Letter == ['n', 'n', 'w', 'n', 'w', 'n', 'n', 'w', 'n'];
+        s = 'P'
+          
+    elseif Letter == ['n', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'w'];
+        s = 'Q'
+          
+    elseif Letter == ['w', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'n'];
+        s = 'R'
+          
+    elseif Letter == ['n', 'n', 'w', 'n', 'n', 'n', 'w', 'w', 'n'];
+        s = 'S'
+          
+    elseif Letter == ['n', 'n', 'n', 'n', 'w', 'n', 'w', 'w', 'n'];
+        s = 'T'
+          
+    elseif Letter == ['w', 'w', 'n', 'n', 'n', 'n', 'n', 'n', 'w'];
+        s = 'U'
+          
+    elseif Letter == ['n', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'w'];
+        s = 'V'
+          
+    elseif Letter == ['w', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'n'];
+        s = 'W'
+          
+    elseif Letter == ['n', 'w', 'n', 'n', 'w', 'n', 'n', 'n', 'w'];
+        s = 'X'
+          
+    elseif Letter == ['w', 'w', 'n', 'n', 'w', 'n', 'n', 'n', 'n'];
+        s = 'Y'
+          
+    elseif Letter == ['n', 'w', 'w', 'n', 'w', 'n', 'n', 'n', 'n'];
+        s = 'Z'
+          
+    elseif Letter == ['n', 'w', 'n', 'n', 'n', 'n', 'w', 'n', 'w'];
+        s = '-'
+          
+    elseif Letter == ['w', 'w', 'n', 'n', 'n', 'n', 'w', 'n', 'n'];
+        s = '/'
+          
+    elseif Letter == ['n', 'w', 'w', 'n', 'n', 'n', 'w', 'n', 'n'];
+        s = ' '
+          
+    elseif Letter == ['n', 'w', 'n', 'w', 'n', 'w', 'n', 'n', 'n'];
+        s = '$'
+          
+    elseif Letter == ['n', 'w', 'n', 'w', 'n', 'n', 'n', 'w', 'n'];
+        s = '/'
+          
+    elseif Letter == ['n', 'w', 'n', 'n', 'n', 'w', 'n', 'w', 'n'];
+        s = '+'
+          
+    elseif Letter == ['n', 'n', 'n', 'w', 'n', 'w', 'n', 'w', 'n'];
+        s = '%'
+          
+    elseif Letter == ['n', 'w', 'n', 'n', 'w', 'n', 'w', 'n', 'n'];
+        s = '*'
+          
     end
     
     beginningOfWide = beginningOfWide + 9;
-
-    % each character in the barcode picture. 
-    Output = [Output s] % add each output character right next to each other. 
+    outputIndex = outputIndex + 1;
 end
-
-Output
